fdfb41c3e48bc5e8bcb3fc87e666ed4764dc3fe1
maximbrail <maxabobus953@gmail.com>
2024-07-24 20:50:15 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let a = prompt("Введите число")
+
+if( a > 0 ){
+    console.log('Это положительное число')
+} else if ( a < 0 ){
+    console.log('Это отрицательное число');
+} else{
+    console.log('Это 0');
+}
